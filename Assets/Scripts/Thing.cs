e5cb4ebb50d7798c7ecccb5259c3e372b0216a1b
Yidi Zhu <yidizhu@halitegames.com>
2017-04-13 21:36:28 +0800

    Implement the mechanic as in "Empty"

---
+++ Assets/Scripts/Thing.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thing : MonoBehaviour {
+
+	public string color;
+    public List<GameObject> startPoints = new List<GameObject>();
+
+    void Start() {
+        int i = 1;
+        while (this.transform.Find("Point (" + i + ")") != null) {
+            startPoints.Add(this.transform.Find("Point (" + i + ")").gameObject);
+            i++;
+        }
+    }
+
+    void Update() {
+        bool areAllSameColor = true;
+
+        foreach (GameObject point in startPoints) {
+            Vector3 direction = point.transform.position - Camera.main.transform.position;
+            direction = direction.normalized;
+
+            Ray ray = new Ray(Camera.main.transform.position, direction);
+
+            RaycastHit hit;
+            if ( Physics.Raycast(ray, out hit, 100f, LayerMask.GetMask("Background")) ) {
+                if (color != hit.collider.gameObject.GetComponent<Background>().color) {
+                    areAllSameColor = false; // if any of these point gives a false, do not remove the thing
+                    Debug.Log(hit.collider.gameObject.GetComponent<Background>().color);
+                } else {
+                    Debug.Log(hit.collider.gameObject.GetComponent<Background>().color);
+                }
+            } else {
+                areAllSameColor = false;
+                Debug.Log("No Background");
+            }
+        }
+
+        if (areAllSameColor) {
+            Destroy(this.gameObject);
+        }
+    }
+}
